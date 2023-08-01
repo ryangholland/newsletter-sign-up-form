81896ef3c26bbd18049980bde38f541a3e1416f0
ryangholland <rghcode@gmail.com>
2023-08-01 18:14:46 -0400

    Add custom validation for email address input

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const subscribeBtn = document.querySelector("[data-subscribe-btn]");
+const subscribeForm = document.querySelector("[data-subscribe-form]");
 const dismissBtn = document.querySelector("[data-dismiss-btn]");
 const mainContainer = document.querySelector("[data-main-container]");
 const successContainer = document.querySelector("[data-success-container]");
+const emailInput = document.querySelector("[data-email-input]");
+const formError = document.querySelector("[data-form-error");
 
-subscribeBtn.addEventListener("click", (e) => {
+subscribeForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  mainContainer.hidden = true;
-  successContainer.hidden = false;
+  if (emailInput.validity.valueMissing || emailInput.validity.typeMismatch) {
+    formError.textContent = "Valid email required";
+    emailInput.classList.add("invalid-input");
+  } else {
+    mainContainer.hidden = true;
+    successContainer.hidden = false;
+    emailInput.value = "";
+  }
 });
+
+emailInput.addEventListener("input", (e) => {
+    emailInput.classList.remove("invalid-input")
+    formError.textContent = "";
+})
 
 dismissBtn.addEventListener("click", (e) => {
   mainContainer.hidden = false;
